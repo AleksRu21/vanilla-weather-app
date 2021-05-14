19d0203f84f5e72d8a20a524ed0e727b1e2c671e
AleksRu21 <sasharudyak92@gmail.com>
2021-05-14 21:25:57 +0200

    Current temperature and weather state

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 function displayTemperature(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "d4d65b2d65c67ac793e30f32ff741c0b";
